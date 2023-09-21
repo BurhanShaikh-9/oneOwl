3ddf79d9fd3dbad0f38a68cac11d202204c4580f
Nyx-95 <shaikhshady6@gmail.com>
2023-09-21 23:14:23 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,12 @@ import { DarkMode } from './services/darkMode';
 import ReactGA from "react-ga4";
 
 
-
 const TRACKING_ID = "G-3L133H7SGZ"
-ReactGA.initialize([
-  {
-    trackingId: TRACKING_ID,
-  }
-]);
+ReactGA.initialize(TRACKING_ID);
+
 function App() {
 
-  // const { getDarkMode } = DarkMode();
+  const { getDarkMode } = DarkMode();
   const [userState, setUserState] = useState('');
   const [userStateLast, setUserStateLast] = useState('');
   const getUserNameVal = (data) => {
@@ -47,12 +43,17 @@ function App() {
 
   const location = useLocation();
   useEffect(() => {
-    const locationss = location.pathname + location.search
-    ReactGA.send({ hitType: "pageview", page: locationss, title: locationss });
-
+    const locations = location.pathname + location.search
+    ReactGA.send({ hitType: "pageview", page: locations, title: locations });
   }, [location.pathname, location.search]);
 
-
+  const trackEvent = (category, action, label) => {
+    ReactGA.event({
+      category,
+      action,
+      label,
+    });
+  };
 
   return (
     <React.Fragment>
@@ -60,15 +61,15 @@ function App() {
         <ThemeContext.Provider value={isDarkMode}>
           <Routes>
             <Route element={<Layout />}>
-              <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-              <Route path={ROUTES.COMMUNICATION} element={<Communication />} />
-              <Route path={ROUTES.FAQ} element={<Faq />} />
-              <Route path={ROUTES.PREFERENCE} element={<Prefernce />} />
-              <Route path={ROUTES.BILLING} element={<Billing />} />
-              <Route path={ROUTES.NOTIFICATION} element={<Notification />} />
-              <Route path={ROUTES.SETTINGS} element={<Settings getThemeColor={getThemeColor} />} />
-              <Route path={ROUTES.PROFILE} element={<Profile getUserNameVal={getUserNameVal} getUserLastVal={getUserLastVal} />} />
-              <Route path={ROUTES.PRIVACY} element={<Privacy />} />
+              <Route path={ROUTES.DASHBOARD} element={<Dashboard />} onEnter={() => trackEvent('Page View', 'Dashboard View', 'Dashboard')} />
+              <Route path={ROUTES.COMMUNICATION} element={<Communication />} onEnter={() => trackEvent('Page View', 'Communication View', 'Communication')} />
+              <Route path={ROUTES.FAQ} element={<Faq />} onEnter={() => trackEvent('Page View', 'FAQ View', 'FAQ')} />
+              <Route path={ROUTES.PREFERENCE} element={<Prefernce />} onEnter={() => trackEvent('Page View', 'Preference View', 'Preference')} />
+              <Route path={ROUTES.BILLING} element={<Billing />} onEnter={() => trackEvent('Page View', 'Billing View', 'Billing')} />
+              <Route path={ROUTES.NOTIFICATION} element={<Notification />} onEnter={() => trackEvent('Page View', 'Notification View', 'Notification')} />
+              <Route path={ROUTES.SETTINGS} element={<Settings getThemeColor={getThemeColor} />} onEnter={() => trackEvent('Page View', 'Settings View', 'Settings')} />
+              <Route path={ROUTES.PROFILE} element={<Profile getUserNameVal={getUserNameVal} getUserLastVal={getUserLastVal} />} onEnter={() => trackEvent('Page View', 'Profile View', 'Profile')} />
+              <Route path={ROUTES.PRIVACY} element={<Privacy />} onEnter={() => trackEvent('Page View', 'Privacy View', 'Privacy')} />
             </Route>
           </Routes>
         </ThemeContext.Provider>
