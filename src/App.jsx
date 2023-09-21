fab33109ad7a908f5c295fd218369528aaa33f5a
Nyx-95 <shaikhshady6@gmail.com>
2023-09-21 22:26:39 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './styles/responsive.css'
 import { Sidebar } from './Website/components/sidebar';
 // import 'react-toastify/dist/ReactToastify.css';
 // import { ExternalRoutes } from './routing/routes';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { ROUTES } from '../utils/routes';
 import { Dashboard } from './Website/pages/Dashboard/dashboard'
 import { Layout } from './Website/layout'
@@ -24,10 +24,9 @@ import ReactGA from 'react-ga';
 const TRACKING_ID = "G-3L133H7SGZ"
 ReactGA.initialize(TRACKING_ID);
 
-
 function App() {
 
-  const {getDarkMode} = DarkMode();
+  const { getDarkMode } = DarkMode();
   const [userState, setUserState] = useState('');
   const [userStateLast, setUserStateLast] = useState('');
   const getUserNameVal = (data) => {
@@ -42,41 +41,35 @@ function App() {
     setIsDarkMode(data)
   }
 
-  useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.style.backgroundColor = '#222222';
-    } 
-    return () => {
-      document.documentElement.style.backgroundColor = ''; 
-    };
-  }, [isDarkMode]);
-
-  useEffect(()=>{
-    setIsDarkMode(getDarkMode())
-  },[])
-
-
+  const location = useLocation();
 
   useEffect(() => {
-    const currentPath = window.location.pathname + window.location.search;
-    ReactGA.pageview(currentPath);
-  }, []);
-  
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location.pathname, location.search]);
+
+  const trackEvent = (category, action, label) => {
+    ReactGA.event({
+      category,
+      action,
+      label,
+    });
+  };
+
   return (
     <React.Fragment>
       <UserNameContext.Provider value={[userState, userStateLast]}>
         <ThemeContext.Provider value={isDarkMode}>
           <Routes>
             <Route element={<Layout />}>
-              <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-              <Route path={ROUTES.COMMUNICATION} element={<Communication />} />
-              <Route path={ROUTES.FAQ} element={<Faq />} />
-              <Route path={ROUTES.PREFERENCE} element={<Prefernce />} />
-              <Route path={ROUTES.BILLING} element={<Billing />} />
-              <Route path={ROUTES.NOTIFICATION} element={<Notification />} />
-              <Route path={ROUTES.SETTINGS} element={<Settings getThemeColor={getThemeColor}/>} />
-              <Route path={ROUTES.PROFILE} element={<Profile getUserNameVal={getUserNameVal} getUserLastVal={getUserLastVal} />} />
-              <Route path={ROUTES.PRIVACY} element={<Privacy />} />
+              <Route path={ROUTES.DASHBOARD} element={<Dashboard />}    onEnter={() => trackEvent('Page View', 'Dashboard View', 'Dashboard')}/>
+              <Route path={ROUTES.COMMUNICATION} element={<Communication/>}  onEnter={() => trackEvent('Page View', 'Communication View', 'Communication')}/>
+              <Route path={ROUTES.FAQ} element={<Faq />}   onEnter={() => trackEvent('Page View', 'FAQ View', 'FAQ')}/>
+              <Route path={ROUTES.PREFERENCE} element={<Prefernce />}   onEnter={() => trackEvent('Page View', 'Preference View', 'Preference')}/>
+              <Route path={ROUTES.BILLING} element={<Billing />} onEnter={() => trackEvent('Page View', 'Billing View', 'Billing')}/>
+              <Route path={ROUTES.NOTIFICATION} element={<Notification />} onEnter={() => trackEvent('Page View', 'Notification View', 'Notification')}/>
+              <Route path={ROUTES.SETTINGS} element={<Settings getThemeColor={getThemeColor} />} onEnter={() => trackEvent('Page View', 'Settings View', 'Settings')}/>
+              <Route path={ROUTES.PROFILE} element={<Profile getUserNameVal={getUserNameVal} getUserLastVal={getUserLastVal} />} onEnter={() => trackEvent('Page View', 'Profile View', 'Profile')}/>
+              <Route path={ROUTES.PRIVACY} element={<Privacy />} onEnter={() => trackEvent('Page View', 'Privacy View', 'Privacy')}/>
             </Route>
           </Routes>
         </ThemeContext.Provider>
